77083c70906ce565f9d2f770e5caeece38e32ffc
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-06-21 22:46:29 -0700

    Switch to a system of separate scripts.

---
+++ get_deps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+USE_LEGION=${USE_LEGION:-1}
+
+if [[ -e deps ]]; then
+    echo "The directory deps already exists, nothing to do."
+    echo "Delete deps and run again if you want to re-download dependencies."
+    false
+fi
+
+mkdir deps
+
+if [[ $USE_LEGION -eq 1 ]]; then
+    export LEGION_DIR=$PWD/deps/legion
+    cat >>deps/env.sh <<EOF
+export USE_LEGION=$USE_LEGION
+export LG_RT_DIR=$LEGION_DIR/runtime
+EOF
+    source deps/env.sh
+    git clone https://gitlab.com/StanfordLegion/legion.git $LEGION_DIR
+fi
