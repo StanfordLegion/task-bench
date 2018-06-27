ef87a0849c67b4879974401e0fd3975714117b3d
fuyuan <fuyuan@iu.edu>
2018-06-27 17:50:45 -0600

    clean timer code

--- legion/main.cc
+++ legion/main.cc
@@ -201,33 +201,16 @@ void top(const Task *task,
 
   //warm up
   // app.execute_main_loop();
-  // Execution fence to wait for all prior operations to be done before getting our timing result
-  runtime->issue_execution_fence(ctx);
 
   printf("\nAfter warm up, Starting main simulation loop\n");
-  // bool simulation_success = true;
-  // Future f_start = runtime->get_current_time_in_microseconds(ctx);
-  // double ts_start = f_start.get_result<long long>();
-
+  // Execution fence to wait for all prior operations to be done before getting our timing result
   Timer::sync_time_start();
 
   app.execute_main_loop();
 
-  // Execution fence to wait for all prior operations to be done before getting our timing result
-  // runtime->issue_execution_fence(ctx);
-  // Future f_end = runtime->get_current_time_in_microseconds(ctx);
-  // double ts_end = f_end.get_result<long long>();
-  // if (simulation_success)
-  //   printf("SUCCESS!\n");
-  // else
-  //   printf("FAILURE!\n");
-  // {
-  //   double sim_time = 1e-6 * (ts_end - ts_start);
-  //   printf("ELAPSED TIME = %7.3f s\n", sim_time);
-  // }
-
+  // Execution fence timer
   double t_elapsed = Timer::sync_time_end();
-  printf("[****] TIME(s) %12.5f\n", t_elapsed/1e6);
+  printf("[****] TIME(s) %12.5f\n", t_elapsed * 1e-6);
 
 }
 
