43763e6156bb221e4c6675e0c1cd9f0b54281199
wkautz <30605516+wkautz@users.noreply.github.com>
2018-07-03 14:37:37 -0700

    Update test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -13,6 +13,7 @@ source deps/env.sh
 if [[ $USE_LEGION -eq 1 ]]; then
     ./legion/task_bench -steps 9 -type dom
 fi
+
 if [[ $TASKBENCH_USE_MPI -eq 1 ]]; then
-    ./mpi/basic -type stencil_1d
+    ./mpi/basic -width 1 -type stencil_1d
 fi
