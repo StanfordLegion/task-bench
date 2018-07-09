77e42975b234aaab31f0e1bba0ebe94c5e5ced66
wkautz <30605516+wkautz@users.noreply.github.com>
2018-07-09 10:24:45 -0700

    created bcast_general

--- mpi/bcast.cc
+++ mpi/bcast.cc
@@ -1,31 +1,50 @@
-/* Copyright 2018 Stanford University                                                                                                                                            
- *                                                                                                                                                                               
- * Licensed under the Apache License, Version 2.0 (the "License");                                                                                                               
- * you may not use this file except in compliance with the License.                                                                                                              
- * You may obtain a copy of the License at                                                                                                                                       
- *                                                                                                                                                                               
- *     http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                
- *                                                                                                                                                                               
- * Unless required by applicable law or agreed to in writing, software                                                                                                           
- * distributed under the License is distributed on an "AS IS" BASIS,                                                                                                             
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                                                                                      
- * See the License for the specific language governing permissions and                                                                                                           
- * limitations under the License.                                                                                                                                                
- */
-
+/* Currently forces a task to depend on itself */
+/* Might be able to hard code against those */
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "../core/core.h"
+#include "../core/timer.h"
 #define  MASTER 0
 
-/* This program uses broadcast to simulate a stencil_1d dependency graph */
+int count_dependencies(std::vector< std::pair<long, long> > &dep)
+{
+  int total_dependencies = 0;
+  for (std::pair<int, int> interval: dep)
+    {
+      for (int i = interval.first; i <= interval.second; i++)
+        total_dependencies++;
+    }
+  return total_dependencies;
+}
+
+void free_all_comms(std::vector< std::vector<std::vector <std::pair<int, MPI_Comm> > > > graph_all_comms)
+{
+  for (std::vector<std::vector <std::pair<int, MPI_Comm> > > dset: graph_all_comms)
+    {
+      for (std::vector <std::pair<int, MPI_Comm> > all_comm: dset)
+		{
+	  		for (std::pair<int, MPI_Comm> comm: all_comm)
+	    		MPI_Comm_free(&comm.second);
+		}
+    }
+}
+
+/* Still causes segfaults */
+void free_all_data (std::vector<std::vector<int *> > all_data)
+{
+  for (std::vector<int *> dset_data: all_data)
+    {
+      for (int *data: dset_data)
+		if (data != NULL) free(data);
+    }
+}
+
 int main (int argc, char *argv[])
 {
   int numtasks, taskid;
   MPI_Status status;
 
-  /* Initialize MPI */
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
   MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
@@ -34,118 +53,175 @@ int main (int argc, char *argv[])
   MPI_Comm_group(MPI_COMM_WORLD, &world_group);
 
   App new_app(argc, argv);
-  std::vector<TaskGraph> graphs = new_app.graphs;
-
-  /* Data to scatter */
-  int num_sent = (taskid != 0 && taskid != numtasks - 1) ? 3 : 2;
-  int data_to_send = taskid;
-
-  /* Data to receive from scatter */
-  int data_to_receive1 = -1;
-  int data_to_receive2 = -1;
-  int data_to_receive3 = taskid;
   
-  /* Receive Scatter Comm */
-  int *first_ranks = NULL;
-  MPI_Group first_group;
-  MPI_Comm first_comm;
-  int first_rank;
-  if (taskid != 0)
+  /* Preallocated storage needed for all the graphs */
+  std::vector<TaskGraph> graphs = new_app.graphs;
+  std::vector<std::vector<int *> > graph_data_to_receive;
+  std::vector< std::vector<std::vector <std::pair<int, MPI_Comm> > > > graph_all_comms;
+  
+  /* Loop through all graphs */
+  for (size_t graph_num = 0; graph_num < graphs.size(); graph_num++)
     {
-      int num_in_group = (taskid == 1) ? 2 : 3;
-      first_ranks = (int *)malloc(sizeof(int) * num_in_group); //check to make sure not null                                                                                     
-      if (num_in_group == 3)
-        {
-          first_ranks[0] = taskid - 2;
-          first_ranks[1] = taskid - 1;
-          first_ranks[2] = taskid;
-        }
-      else
-        {
-          first_ranks[0] = taskid - 1;
-          first_ranks[1] = taskid;
-        }
-      MPI_Group_incl(world_group, num_in_group, first_ranks, &first_group);
-
-      MPI_Comm_create_group(MPI_COMM_WORLD, first_group, 0, &first_comm);
-      MPI_Comm_rank(first_comm, &first_rank);
+      TaskGraph graph = graphs[graph_num];
+      
+      /* Preallocated storage needed for all dsets in each graph */
+      std::vector<int *> dset_data_to_receive;
+      std::vector<std::vector <std::pair<int, MPI_Comm> > > dset_all_comms;
+      
+      /* Loop through all dsets for each graph */
+      for (long dset = 0L; dset < graph.max_dependence_sets(); dset++)
+		{
+		  bool loop = false;
+		  bool inner_loop = false;
+		  bool seen_self = false;
+		  
+		  /* Preallocated data for each dset */
+		  std::vector<std::pair<int, MPI_Comm> > all_comms;
+		  std::vector<std::pair<long, long> > dependencies = graph.dependencies(dset, taskid);
+		  int num_dependencies = count_dependencies(dependencies);
+		  int *data_to_receive = (int *)malloc(sizeof(int) * num_dependencies);
+		  
+		  for (std::pair<long, long> interval: dependencies)
+		    {
+		      /* Special case if the graph involves wrapping to make sure the order of                                                          
+			 the "MPI_Comm_create_group" calls do not hang */
+		      if (graph.dependence == DependenceType::STENCIL_1D_PERIODIC)
+				{
+			  		if ((taskid == numtasks - 1) && !loop) {
+			    		loop = true;
+			    		interval = dependencies[1];
+			  		} else if ((taskid == numtasks - 1) && loop) interval = dependencies[0];
+				}
+		      
+		      for (int i = interval.first; i <= interval.second; i++)
+				{
+			 	 if (i == taskid) seen_self = true;
+			  
+			  		std::vector< std::pair<long, long> > rev_dependencies = graph.reverse_dependencies(dset, i);
+			  		int num_rev_dependencies = count_dependencies(rev_dependencies);
+			  
+			  		int *recv_ranks = num_rev_dependencies != 0 ? (int *)malloc(sizeof(int) * num_rev_dependencies) : NULL;
+			  		
+			  		bool inner_seen = false;
+			  		int index = 0;
+			  		int root_of_recv = -1;
+		  
+			  		for (std::pair<long, long> inner_interval: rev_dependencies)
+			  		  {
+			  		    /* Special case if the graph involves wrapping to make sure the order of
+						 the "MPI_Comm_create_group" calls do not hang */
+			  		    if (graph.dependence == DependenceType::STENCIL_1D_PERIODIC)
+						{
+						  if (i == numtasks - 1 && !inner_loop) 
+						    {
+						      inner_loop = true;
+						      inner_interval = rev_dependencies[1];
+						    }
+						  else if (i == numtasks - 1 && inner_loop) inner_interval = rev_dependencies[0];
+						}
+			    	  for (int inter = inner_interval.first; inter <= inner_interval.second; inter++)
+						{
+				 		 if (inter == i) 
+				   			 {
+				    		  inner_seen = true;
+				    		  root_of_recv = index;
+				   			 }
+				 		 recv_ranks[index] = inter;
+				 		 index++;
+						}
+			   		  }
+			  /* Forces a task to be in the communicator of its reverse dependencies */
+			  		if (!inner_seen)
+			   		 {
+			      		recv_ranks[index] = i;
+			      		root_of_recv = index;
+			     		 num_rev_dependencies++;
+			    	 }
+		      
+			  		MPI_Group recv_group;
+			  		MPI_Group_incl(world_group, num_rev_dependencies, recv_ranks, &recv_group);
+			  		free(recv_ranks); //tentatively
+		  	  		
+			  		MPI_Comm recv_comm;
+			  		MPI_Comm_create_group(MPI_COMM_WORLD, recv_group, 0, &recv_comm);
+			  		MPI_Group_free(&recv_group); //tentatively
+			  		std::pair<int, MPI_Comm> final_comm = std::make_pair(root_of_recv, recv_comm);
+			  		all_comms.push_back(final_comm);	  
+				}
+		    }
+		  /* Forces a task to include itself as a dependency to keep communicators consistent */
+		  if (!seen_self)
+		    {
+		      std::vector< std::pair<long, long> > rev_dependencies = graph.reverse_dependencies(dset, taskid);
+		      int num_rev_dependencies = count_dependencies(rev_dependencies);
+    	                                                                                 
+		      int *recv_ranks = (int *)malloc(sizeof(int) * num_rev_dependencies);
+		      int index = 0;
+		      int root_of_recv = -1;
+		      for (std::pair<long, long> inner_interval: rev_dependencies)
+				{
+			 	 for (int inter = inner_interval.first; inter <= inner_interval.second; inter++)
+			   		{
+			      		recv_ranks[index] = inter;
+			      		index++;
+			    	}
+				}
+		    	MPI_Group recv_group;
+		      	MPI_Comm recv_comm1;
+		      	if (num_rev_dependencies != 0)
+				  {
+			  		recv_ranks[index] = taskid;
+			  		num_rev_dependencies++;
+			  		root_of_recv = index;
+				  }
+		     	 MPI_Group_incl(world_group, num_rev_dependencies, recv_ranks, &recv_group);
+		      	free(recv_ranks); //tentatively
+		      	MPI_Comm_create_group(MPI_COMM_WORLD, recv_group, 0, &recv_comm1);
+		      	MPI_Group_free(&recv_group); //tentatively
+		 
+		      	std::pair<int, MPI_Comm> final_comm = std::make_pair(root_of_recv, recv_comm1);
+		      	all_comms.push_back(final_comm);
+		    }
+		  dset_all_comms.push_back(all_comms);
+		  dset_data_to_receive.push_back(data_to_receive);
+		}
+      graph_all_comms.push_back(dset_all_comms);
+      graph_data_to_receive.push_back(dset_data_to_receive);
     }
 
-  /* Send Scatter Comm */
-  int *second_ranks = (int *)malloc(sizeof(int) * num_sent);
-  int second_rank;
-  if (taskid == 0)
+  struct Timer__<struct MPITimer> timer;
+  timer.sync_time_start();
+
+  for (size_t graph_num = 0; graph_num < graphs.size(); graph_num++)
     {
-      second_ranks[0] = 0;
-      second_ranks[1] = 1;
-    }
-  else if (taskid == numtasks - 1)
-    {
-      second_ranks[0] = numtasks - 2;
-      second_ranks[1] = numtasks - 1;
-    }
-  else
-    {
-      second_ranks[0] = taskid - 1;
-      second_ranks[1] = taskid;
-      second_ranks[2] = taskid + 1;
-    }
-     MPI_Group second_group;
-  MPI_Group_incl(world_group, num_sent, second_ranks, &second_group);
-
-  MPI_Comm second_comm;
-  MPI_Comm_create_group(MPI_COMM_WORLD, second_group, 0, &second_comm);
-  MPI_Comm_rank(second_comm, &second_rank);
-
-
-  /* Second Receive Scatter Comm */
-  int *third_ranks;
-  int third_rank;
-  MPI_Group third_group;
-  MPI_Comm third_comm;
-  if (taskid != numtasks - 1)
-    {
-      int num_in_group = (taskid == numtasks - 2) ? 2 : 3;
-      third_ranks = (int *)malloc(sizeof(int) * num_in_group);
-
-      third_ranks[0] = taskid;
-      third_ranks[1] = taskid + 1;
-      if (num_in_group == 3) third_ranks[2] = taskid + 2;
-      MPI_Group_incl(world_group, num_in_group, third_ranks, &third_group);
-
-      MPI_Comm_create_group(MPI_COMM_WORLD, third_group, 0, &third_comm);
-      MPI_Comm_rank(third_comm, &third_rank);
-    }
-
-  //TIMER START
-  for (TaskGraph graph: graphs)
-    {
+      //extra time copying the graph
+      TaskGraph graph = graphs[graph_num];
       Kernel k(graph.kernel);
       for (long timestep = 0L; timestep < graph.timesteps; timestep++)
-        {
-          /* Kernel Execute Call */
-          k.execute();
-
-          MPI_Bcast(&data_to_send, 1, MPI_INT, second_rank, second_comm); /* Send to comm */
-          if (taskid != MASTER) MPI_Bcast(&data_to_receive1, 1, MPI_INT, first_rank - 1, first_comm); /* Receive from left neighbor */
-          if (taskid != numtasks - 1) MPI_Bcast(&data_to_receive2, 1, MPI_INT, 1, third_comm); /* Receive from right neighbor */
-        }                                                                                                                                                                
+		{
+		  //extra time with api calls
+		  long dset = graph.dependence_set_at_timestep(timestep);
+      k.execute();
+		  
+		  for(int i = 0; i < graph_all_comms[graph_num][dset].size(); i++)     
+		    {
+		      if (taskid >= graph.offset_at_timestep(timestep) 
+		      		&& taskid < graph.offset_at_timestep(timestep) + graph.width_at_timestep(timestep) 
+			  		&& graph_all_comms[graph_num][dset][i].first != -1)
+				{
+			  		int data = taskid;
+			  		MPI_Bcast(&data, 1, MPI_INT, graph_all_comms[graph_num][dset][i].first, graph_all_comms[graph_num][dset][i].second);
+			  		graph_data_to_receive[graph_num][dset][i - 1] = data;
+				}
+	    	}
+		}  
     }
-    //Timer end and record
+  double time_elapsed = timer.sync_time_end();
+  if (taskid == MASTER) new_app.report_timing(time_elapsed);
 
   /* Free all memory */
   MPI_Group_free(&world_group);
-  if (taskid != 0)
-    {
-      MPI_Group_free(&first_group);
-      MPI_Comm_free(&first_comm);
-    }
-  MPI_Group_free(&second_group);
-  MPI_Comm_free(&second_comm);
-  if (taskid != numtasks - 1)
-    {
-      MPI_Group_free(&third_group);
-      MPI_Comm_free(&third_comm);
-    }
+  free_all_comms(graph_all_comms);
+  //free_all_data(graph_data_to_receive); //still need to do, some are null
   MPI_Finalize();
 }
