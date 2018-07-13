f3fa5e8a0d60b22206574d8bf0b425f2544e05ba
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-07-13 09:33:11 -0700

    Update script for measuring EMTG with Legion.

--- scripts/sherlock/emtg_legion.sh
+++ scripts/sherlock/emtg_legion.sh
@@ -8,7 +8,22 @@
 #SBATCH --mail-type=ALL
 
 (for s in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
-     srun --cpu_bind none ./task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 18 -fields 2 -ll:cpu 18 -ll:util 0 -dm:memoize -lg:parallel_replay 18
- done) | tee scaling_memoize.txt
+     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 18 -fields 2 -ll:cpu 18 -ll:util 0 -dm:memoize -lg:parallel_replay 18
+ done) | tee scaling_memoize_util_0.txt
 
-grep Elapsed scaling_memoize.txt | cut -d' ' -f 3
+(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
+     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 18 -fields 2 -ll:cpu 18 -ll:util 1 -ll:pin_util -dm:memoize
+ done) | tee scaling_memoize_util_1.txt
+
+(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
+     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 18 -fields 2 -ll:cpu 18 -ll:util 2 -dm:memoize
+ done) | tee scaling_memoize_util_2.txt
+
+echo scaling_memoize_util_0.txt
+grep Elapsed scaling_memoize_util_0.txt | cut -d' ' -f 3
+
+echo scaling_memoize_util_1.txt
+grep Elapsed scaling_memoize_util_1.txt | cut -d' ' -f 3
+
+echo scaling_memoize_util_2.txt
+grep Elapsed scaling_memoize_util_2.txt | cut -d' ' -f 3
