20d3b01a1fc7959e6ef3aa0078f9665ab13a209d
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-07-19 13:21:17 -0700

    Run more iterations in EMTG scripts.

--- scripts/sherlock/emtg_legion.sh
+++ scripts/sherlock/emtg_legion.sh
@@ -7,23 +7,23 @@
 #SBATCH --time=01:00:00
 #SBATCH --mail-type=ALL
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
-     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 18 -fields 2 -ll:cpu 18 -ll:util 0 -dm:memoize -lg:parallel_replay 18
- done) | tee scaling_memoize_util_0.txt
+(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 18 -fields 2 -ll:cpu 18 -ll:util 0 -dm:memoize -lg:parallel_replay 18
+ done) | tee legion_util_0.txt
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
-     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 18 -fields 2 -ll:cpu 18 -ll:util 1 -ll:pin_util -dm:memoize
- done) | tee scaling_memoize_util_1.txt
+(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 18 -fields 2 -ll:cpu 18 -ll:util 1 -ll:pin_util -dm:memoize
+ done) | tee legion_util_1.txt
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
-     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 18 -fields 2 -ll:cpu 18 -ll:util 2 -dm:memoize
- done) | tee scaling_memoize_util_2.txt
+(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 18 -fields 2 -ll:cpu 18 -ll:util 2 -dm:memoize
+ done) | tee legion_util_2.txt
 
-echo scaling_memoize_util_0.txt
-grep Elapsed scaling_memoize_util_0.txt | cut -d' ' -f 3
+echo legion_util_0.txt
+grep Elapsed legion_util_0.txt | cut -d' ' -f 3
 
-echo scaling_memoize_util_1.txt
-grep Elapsed scaling_memoize_util_1.txt | cut -d' ' -f 3
+echo legion_util_1.txt
+grep Elapsed legion_util_1.txt | cut -d' ' -f 3
 
-echo scaling_memoize_util_2.txt
-grep Elapsed scaling_memoize_util_2.txt | cut -d' ' -f 3
+echo legion_util_2.txt
+grep Elapsed legion_util_2.txt | cut -d' ' -f 3
--- scripts/sherlock/emtg_starpu.sh
+++ scripts/sherlock/emtg_starpu.sh
@@ -7,8 +7,8 @@
 #SBATCH --time=01:00:00
 #SBATCH --mail-type=ALL
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12; do
-     srun --cpu_bind none ../../starpu/main -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 100 -width 20 -core 20
+(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+     srun --cpu_bind none ../../starpu/main -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 20 -core 20
  done) | tee starpu.txt
 
 grep Elapsed starpu.txt | cut -d' ' -f 3
