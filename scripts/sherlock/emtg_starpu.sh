26af21e6485af4b0a8c6250a530dbb570dde36dd
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-07-19 23:03:16 -0700

    Update EMTG scripts for parsec and starpu.

--- scripts/sherlock/emtg_starpu.sh
+++ scripts/sherlock/emtg_starpu.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 #SBATCH --partition=aaiken
-#SBATCH --ntasks=1
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --cpus-per-task=20
 #SBATCH --exclusive
 #SBATCH --time=01:00:00
 #SBATCH --mail-type=ALL
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-     srun --cpu_bind none ../../starpu/main -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 20 -core 20
- done) | tee starpu.txt
+function launch {
+    srun -n $1 -N $1 --cpu_bind none ../../starpu/main "${@:2}" -width 20 -core 20
+}
 
-grep Elapsed starpu.txt | cut -d' ' -f 3
+function sweep {
+    for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+        $1 $2 -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800
+    done
+}
+
+for n in 4 2 1; do
+    sweep launch $n > starpu_nodes_$n.log
+done
