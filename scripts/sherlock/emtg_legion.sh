921e0f744df02f8ce76cfe705ca9226f337fc549
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-07-19 23:00:09 -0700

    Update EMTG Legion script.

--- scripts/sherlock/emtg_legion.sh
+++ scripts/sherlock/emtg_legion.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 #SBATCH --partition=aaiken
-#SBATCH --ntasks=1
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --cpus-per-task=20
 #SBATCH --exclusive
 #SBATCH --time=01:00:00
 #SBATCH --mail-type=ALL
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 18 -fields 2 -ll:cpu 18 -ll:util 0 -dm:memoize -lg:parallel_replay 18
- done) | tee legion_util_0.txt
+function launch_util_0 {
+     srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width 18 -fields 2 -ll:cpu 18 -ll:util 0 -dm:memoize -lg:parallel_replay 18
+}
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 18 -fields 2 -ll:cpu 18 -ll:util 1 -ll:pin_util -dm:memoize
- done) | tee legion_util_1.txt
+function launch_util_1 {
+     srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width 18 -fields 2 -ll:cpu 18 -ll:util 1 -ll:pin_util -dm:memoize
+}
 
-(for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
-     srun --cpu_bind none ../../legion/task_bench -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800 -width 18 -fields 2 -ll:cpu 18 -ll:util 2 -dm:memoize
- done) | tee legion_util_2.txt
+function launch_util_2 {
+     srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width 18 -fields 2 -ll:cpu 18 -ll:util 2 -dm:memoize
+}
 
-echo legion_util_0.txt
-grep Elapsed legion_util_0.txt | cut -d' ' -f 3
+function sweep {
+    for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
+        $1 $2 -kernel busy_wait -iter $(( 1 << (24-s) )) -type stencil_1d -steps 800
+    done
+}
 
-echo legion_util_1.txt
-grep Elapsed legion_util_1.txt | cut -d' ' -f 3
-
-echo legion_util_2.txt
-grep Elapsed legion_util_2.txt | cut -d' ' -f 3
+for n in 4 2 1; do
+    sweep launch_util_0 $n > legion_util_0_nodes_$n.log
+    sweep launch_util_1 $n > legion_util_1_nodes_$n.log
+    sweep launch_util_2 $n > legion_util_2_nodes_$n.log
+done
