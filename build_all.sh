ac71f4b830de29fd6bd1fe1975b84a395774953d
Wei Wu <wwu12@saturn.icl.utk.edu>
2018-07-26 18:48:22 -0400

    fix build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -37,11 +37,11 @@ if [[ $USE_LEGION -eq 1 ]]; then
 fi
 
 if [[ $USE_STARPU -eq 1 ]]; then
-    STARPU_CONFIGURE_FLAG = --disable-cuda --disable-opencl --disable-fortran --disable-build-tests --disable-build-examples
+    STARPU_CONFIGURE_FLAG="--disable-cuda --disable-opencl --disable-fortran --disable-build-tests --disable-build-examples"
     if [[ $STARPU_ENABLE_HWLOC -eq 1 ]]; then
-      STARPU_HWLOC_FLAG = 
+      STARPU_CONFIGURE_FLAG+="" 
     else
-      STARPU_CONFIGURE_FLAG += --without-hwloc
+      STARPU_CONFIGURE_FLAG+="--without-hwloc"
     fi 
     pushd "$STARPU_SRC_DIR"
     ./configure --prefix=$STARPU_DIR $STARPU_CONFIGURE_FLAG
