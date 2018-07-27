f56cb666bd03f752e6d6ad062448d4feca390b7b
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-07-27 15:48:44 -0700

    Update Sherlock script for Legion.

--- scripts/sherlock/emtg_legion.sh
+++ scripts/sherlock/emtg_legion.sh
@@ -8,15 +8,27 @@
 cores=$(( SLURM_JOB_CPUS_PER_NODE - 2 ))
 
 function launch_util_0 {
-     srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width $(( $1 * cores )) -fields 2 -ll:cpu $cores -ll:util 0 -dm:memoize -lg:parallel_replay $cores
+    memoize=
+    if [[ $1 -le 1 ]]; then
+        memoize="-dm:memoize -lg:parallel_replay $cores"
+    fi
+     srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width $(( $1 * cores )) -fields 2 -ll:cpu $cores -ll:util 0 $memoize
 }
 
 function launch_util_1 {
-     srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width $(( $1 * cores )) -fields 2 -ll:cpu $cores -ll:util 1 -ll:pin_util -dm:memoize
+    memoize=
+    if [[ $1 -le 1 ]]; then
+        memoize="-dm:memoize"
+    fi
+    srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width $(( $1 * cores )) -fields 2 -ll:cpu $cores -ll:util 1 -ll:pin_util $memoize
 }
 
 function launch_util_2 {
-     srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width $(( $1 * cores )) -fields 2 -ll:cpu $cores -ll:util 2 -dm:memoize
+    memoize=
+    if [[ $1 -le 1 ]]; then
+        memoize="-dm:memoize"
+    fi
+    srun -n $1 -N $1 --cpu_bind none ../../legion/task_bench "${@:2}" -width $(( $1 * cores )) -fields 2 -ll:cpu $cores -ll:util 2 $memoize
 }
 
 function sweep {
