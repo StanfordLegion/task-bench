a9bee9e639559850bc88d1e7718f2f9eedff8af6
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-08-03 21:38:08 -0700

    Test nearest type.

--- test_all.sh
+++ test_all.sh
@@ -11,7 +11,7 @@ fi
 source deps/env.sh
 
 if [[ $TASKBENCH_USE_MPI -eq 1 ]]; then
-    for t in no_comm stencil_1d stencil_1d_periodic fft; do # FIXME: trivial, dom, tree are broken
+    for t in no_comm stencil_1d stencil_1d_periodic fft nearest; do # FIXME: trivial, dom, tree are broken
         # mpirun -np 4 ./mpi/basic         -steps 9 -width 4 -type $t # FIXME: Freezes
         mpirun -np 4 ./mpi/nonblock      -steps 9 -width 4 -type $t
         mpirun -np 4 ./mpi/bcast         -steps 9 -width 4 -type $t
@@ -20,14 +20,17 @@ if [[ $TASKBENCH_USE_MPI -eq 1 ]]; then
     done
 fi
 
+basic_types="trivial no_comm stencil_1d stencil_1d_periodic dom tree fft nearest"
+extended_types="$basic_types all_to_all"
+
 if [[ $USE_LEGION -eq 1 ]]; then
-    for t in trivial no_comm stencil_1d stencil_1d_periodic dom tree fft all_to_all; do
+    for t in $extended_types; do
         ./legion/task_bench -steps 9 -type $t
     done
 fi
 
 if [[ $USE_STARPU -eq 1 ]]; then
-    for t in trivial no_comm stencil_1d stencil_1d_periodic dom tree fft; do
+    for t in $basic_types; do
         mpirun -np 1 ./starpu/main -steps 9 -type $t
         mpirun -np 4 ./starpu/main -steps 9 -type $t -p 1
         mpirun -np 4 ./starpu/main -steps 9 -type $t -p 2
@@ -37,7 +40,7 @@ if [[ $USE_STARPU -eq 1 ]]; then
 fi
 
 if [[ $USE_PARSEC -eq 1 ]]; then
-    for t in trivial no_comm stencil_1d stencil_1d_periodic dom tree fft; do
+    for t in $basic_types; do
         mpirun -np 1 ./parsec/main -steps 9 -type $t
         mpirun -np 4 ./parsec/main -steps 9 -type $t -p 1
         mpirun -np 4 ./parsec/main -steps 9 -type $t -p 2
@@ -47,7 +50,7 @@ if [[ $USE_PARSEC -eq 1 ]]; then
 fi
 
 if [[ $USE_CHARM -eq 1 ]]; then
-    for t in trivial no_comm stencil_1d stencil_1d_periodic dom tree fft all_to_all; do
+    for t in $extended_types; do
         ./charm++/charmrun +p1 ++mpiexec ./charm++/benchmark -steps 9 -type $t
     done
     rm charmrun.*
