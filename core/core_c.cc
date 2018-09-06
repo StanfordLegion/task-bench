6319049bc13d57d3f5ae98c7897647c70602bb09
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-06 12:50:33 -0700

    Restore const for input_bytes.

--- core/core_c.cc
+++ core/core_c.cc
@@ -106,27 +106,25 @@ interval_list_t task_graph_dependencies(task_graph_t graph, long dset, long poin
 
 void task_graph_execute_point(task_graph_t graph, long timestep, long point,
                               char *output_ptr, size_t output_bytes,
-                              char **input_ptr, size_t *input_bytes,
+                              char **input_ptr, const size_t *input_bytes,
                               size_t n_inputs)
 {
   TaskGraph t(graph);
   t.execute_point(timestep, point, output_ptr, output_bytes,
                   const_cast<const char **>(input_ptr),
-                  const_cast<const size_t *>(input_bytes),
-                  n_inputs);
+                  input_bytes, n_inputs);
 }
 
 void task_graph_execute_point_scratch(task_graph_t graph, long timestep, long point,
                                       char *output_ptr, size_t output_bytes,
-                                      char **input_ptr, size_t *input_bytes,
+                                      char **input_ptr, const size_t *input_bytes,
                                       size_t n_inputs,
                                       char *scratch_ptr, size_t scratch_bytes)
 {
   TaskGraph t(graph);
   t.execute_point(timestep, point, output_ptr, output_bytes,
                   const_cast<const char **>(input_ptr),
-                  const_cast<const size_t *>(input_bytes),
-                  n_inputs,
+                  input_bytes, n_inputs,
                   scratch_ptr, scratch_bytes);
 }
 
