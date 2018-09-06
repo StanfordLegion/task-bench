7eb312346cce80bf90a69d11f8f638831859d47f
legendb317 <40677673+legendb317@users.noreply.github.com>
2018-09-05 22:21:45 -0700

    Update emtg_chapel.sh

--- chapel/emtg_chapel.sh
+++ chapel/emtg_chapel.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
+# /* Copyright 2018 Stanford University
+#  *
+#  * Licensed under the Apache License, Version 2.0 (the "License");
+#  * you may not use this file except in compliance with the License.
+#  * You may obtain a copy of the License at
+#  *
+#  *     http://www.apache.org/licenses/LICENSE-2.0
+#  *
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+#  */#!/bin/bash
+ 
 #SBATCH --partition=aaiken
 #SBATCH --cpus-per-task=20
 #SBATCH --exclusive
-#SBATCH --time=02:30:00
+#SBATCH --time=00:30:00
 #SBATCH --mail-type=ALL
 
 function launch {
@@ -12,7 +26,7 @@ function launch {
 
 function sweep {
     for s in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18; do
-        for rep in 0 1 2; do
+        for rep in 0; do
             $1 $2 -timesteps 1000 -iter $(( 1 << (24-s) )) -type $3  -kernel busy_wait
         done
     done
