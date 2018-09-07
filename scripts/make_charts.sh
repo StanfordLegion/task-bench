e593e627688914caa957def6c58f873108ff5885
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-07 16:08:01 -0700

    Rename scripts.

--- scripts/make_charts.sh
+++ scripts/make_charts.sh
@@ -4,5 +4,5 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")"
 c=$1
 n=$2
 for f in *_nodes_$n.log; do
-    (set -x; "$script_dir"/emtg_chart.py $f -c $c -n $n)
+    (set -x; "$script_dir"/metg_chart.py $f -c $c -n $n)
 done
