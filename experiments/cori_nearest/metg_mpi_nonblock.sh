600b99747013ac90ba3f1a60e9f1292210888dea
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-17 22:31:14 -0700

    Increase running time of nearest scripts.

--- experiments/cori_nearest/metg_mpi_nonblock.sh
+++ experiments/cori_nearest/metg_mpi_nonblock.sh
@@ -3,7 +3,7 @@
 #SBATCH --qos=regular
 #SBATCH --constraint=haswell
 #SBATCH --exclusive
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mail-type=ALL
 
 cores=$(( $(echo $SLURM_JOB_CPUS_PER_NODE | cut -d'(' -f 1) / 2 ))
