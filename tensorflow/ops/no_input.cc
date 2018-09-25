a8718c036c934c00a45c349a315f7f68602459b1
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-24 21:27:24 -0700

    Reformat.

--- tensorflow/ops/no_input.cc
+++ tensorflow/ops/no_input.cc
@@ -24,56 +24,61 @@
 using namespace tensorflow;
 
 REGISTER_OP("NoInput")
-	.Input("task_graph: uint32")
-	.Input("timestep: int32")
-	.Input("point: int32")
-	.Attr("T: {int8, int16, int32, int64}")
-	.Output("output: T");
+    .Input("task_graph: uint32")
+    .Input("timestep: int32")
+    .Input("point: int32")
+    .Attr("T: {int8, int16, int32, int64}")
+    .Output("output: T");
 
 template <typename T>
 class NoInputOp : public OpKernel {
 public:
+  explicit NoInputOp(OpKernelConstruction* context) : OpKernel(context) {}
 
-	explicit NoInputOp(OpKernelConstruction* context) : OpKernel(context) {}
+  void Compute(OpKernelContext* context) override
+  {
+    // get input tensors
+    const Tensor& task_graph_tensor = context->input(0);
+    task_graph_t task_graph = constructTaskGraph(task_graph_tensor);
 
-	void Compute(OpKernelContext* context) override {
-		// get input tensors
-		const Tensor& task_graph_tensor = context->input(0);
-		task_graph_t task_graph = constructTaskGraph(task_graph_tensor);
+    const Tensor& timestep_tensor = context->input(1);
+    const Tensor& point_tensor = context->input(2);
+    auto timestep_input = timestep_tensor.flat<int32>();
+    auto point_input = point_tensor.flat<int32>();
+    long timestep = timestep_input(0);
+    long point = point_input(0);
+    // TESTING
+    std::cout << "TIMESTEP: " << timestep << " POINT: " << point << std::endl;
 
-		const Tensor& timestep_tensor = context->input(1);
-		const Tensor& point_tensor = context->input(2);
-		auto timestep_input = timestep_tensor.flat<int32>();
-		auto point_input = point_tensor.flat<int32>();
-		long timestep = timestep_input(0);
-		long point = point_input(0);
-		//TESTING
-		std::cout << "TIMESTEP: " << timestep << " POINT: " << point << std::endl;
+    TensorShape output_shape;
+    output_shape.AddDim(2);
 
-		TensorShape output_shape;
-		output_shape.AddDim(2);
+    Tensor* output_tensor = NULL;
+    OP_REQUIRES_OK(context,
+                   context->allocate_output(0, output_shape, &output_tensor));
+    auto output_flat =
+        output_tensor
+            ->flat<T>();  // set output type to any of int8, int16, int32, int64
 
-		Tensor* output_tensor = NULL;
-		OP_REQUIRES_OK(context, context->allocate_output(0, output_shape, &output_tensor));
-		auto output_flat = output_tensor->flat<T>(); // set output type to any of int8, int16, int32, int64
+    output_flat(0) = timestep;  // assign output to correct type
+    output_flat(1) = point;
 
-		output_flat(0) = timestep; // assign output to correct type
-		output_flat(1) = point;
-
-		// Execute Point
-		char *output_ptr = (char *)(&output_flat);
-		size_t output_bytes = sizeof(output_flat);
-		task_graph_execute_point(task_graph, timestep, point, output_ptr, output_bytes, NULL, NULL, 0);
-	}
+    // Execute Point
+    char* output_ptr = (char*)(&output_flat);
+    size_t output_bytes = sizeof(output_flat);
+    task_graph_execute_point(task_graph, timestep, point, output_ptr,
+                             output_bytes, NULL, NULL, 0);
+  }
 };
 
 // REGISTER_KERNEL_BUILDER(Name("NoInput").Device(DEVICE_CPU), NoInputOp)
 
-// Macro for registering operator for different types (int8, int16, int32, int64)
-#define REGISTER_KERNEL(type)											\
-	REGISTER_KERNEL_BUILDER(											\
-		Name("NoInput").Device(DEVICE_CPU).TypeConstraint<type>("T"),	\
-		NoInputOp<type>)
+// Macro for registering operator for different types (int8, int16, int32,
+// int64)
+#define REGISTER_KERNEL(type)                                       \
+  REGISTER_KERNEL_BUILDER(                                          \
+      Name("NoInput").Device(DEVICE_CPU).TypeConstraint<type>("T"), \
+      NoInputOp<type>)
 
 REGISTER_KERNEL(int8);
 REGISTER_KERNEL(int16);
--- tensorflow/ops/two_input.cc
+++ tensorflow/ops/two_input.cc
@@ -24,80 +24,88 @@
 using namespace tensorflow;
 
 REGISTER_OP("TwoInput")
-	.Input("first_input: T")
-	.Input("second_input: T")
-	.Input("task_graph: uint32")
-	.Input("timestep: int32")
-	.Input("point: int32")
-	.Attr("T: {int8, int16, int32, int64}")
-	.Output("output: T");
+    .Input("first_input: T")
+    .Input("second_input: T")
+    .Input("task_graph: uint32")
+    .Input("timestep: int32")
+    .Input("point: int32")
+    .Attr("T: {int8, int16, int32, int64}")
+    .Output("output: T");
 
 template <typename T>
 class TwoInputOp : public OpKernel {
 public:
+  explicit TwoInputOp(OpKernelConstruction* context) : OpKernel(context) {}
 
-	explicit TwoInputOp(OpKernelConstruction* context) : OpKernel(context) {}
+  void Compute(OpKernelContext* context) override
+  {
+    // get input tensors
+    const Tensor& first_input_tensor = context->input(0);   // left-most input
+    const Tensor& second_input_tensor = context->input(1);  // right-most input
+    const Tensor& timestep_tensor =
+        context->input(3);  // timestep where this op is being run
+    const Tensor& point_tensor =
+        context->input(4);  // point where this op is being run
+    // convert tensors to readable arrays
+    auto first_input = first_input_tensor.flat<T>();
+    auto second_input = second_input_tensor.flat<T>();
+    auto timestep_input = timestep_tensor.flat<int32>();
+    auto point_input = point_tensor.flat<int32>();
+    long timestep = timestep_input(0);
+    long point = point_input(0);
+    // TESTING
+    std::cout << "TIMESTEP: " << timestep << " POINT: " << point << std::endl;
 
-	void Compute(OpKernelContext* context) override {
-		// get input tensors
-		const Tensor& first_input_tensor = context->input(0); // left-most input
-		const Tensor& second_input_tensor = context->input(1); // right-most input
-		const Tensor& timestep_tensor = context->input(3); // timestep where this op is being run
-		const Tensor& point_tensor = context->input(4); // point where this op is being run
-		// convert tensors to readable arrays
-		auto first_input = first_input_tensor.flat<T>();
-		auto second_input = second_input_tensor.flat<T>();
-		auto timestep_input = timestep_tensor.flat<int32>();
-		auto point_input = point_tensor.flat<int32>();
-		long timestep = timestep_input(0);
-		long point = point_input(0);
-		//TESTING
-		std::cout << "TIMESTEP: " << timestep << " POINT: " << point << std::endl;
+    // construct task_graph from given values
+    const Tensor& task_graph_tensor = context->input(2);
+    task_graph_t task_graph = constructTaskGraph(task_graph_tensor);
 
-		// construct task_graph from given values
-		const Tensor& task_graph_tensor = context->input(2);
-		task_graph_t task_graph = constructTaskGraph(task_graph_tensor);
+    TensorShape output_shape;
+    output_shape.AddDim(2);
 
-		TensorShape output_shape;
-		output_shape.AddDim(2);
+    Tensor* output_tensor = NULL;
+    OP_REQUIRES_OK(context,
+                   context->allocate_output(0, output_shape, &output_tensor));
+    auto output_flat =
+        output_tensor
+            ->flat<T>();  // set output type to any of int8, int16, int32, int64
 
-		Tensor* output_tensor = NULL;
-		OP_REQUIRES_OK(context, context->allocate_output(0, output_shape, &output_tensor));
-		auto output_flat = output_tensor->flat<T>(); // set output type to any of int8, int16, int32, int64
+    std::pair<long, long> first_input_pair =
+        std::make_pair(first_input(0), first_input(1));
+    std::pair<long, long> second_input_pair =
+        std::make_pair(second_input(0), second_input(1));
 
-		std::pair<long, long> first_input_pair = std::make_pair(first_input(0), first_input(1));
-		std::pair<long, long> second_input_pair = std::make_pair(second_input(0), second_input(1));
+    const std::pair<long, long>* input[2] = {
+        // array of pointers to pairs
+        &first_input_pair, &second_input_pair};
 
-		const std::pair<long, long> *input[2] = { // array of pointers to pairs
-			&first_input_pair, 
-			&second_input_pair
-		};
+    for (auto pair : input) {
+      std::cout << "INPUT: " << pair->first << " " << pair->second << std::endl;
+    }
 
-		for (auto pair : input) {
-			std::cout << "INPUT: " << pair->first << " " << pair->second << std::endl;
-		}
+    output_flat(0) = timestep;
+    output_flat(1) = point;
 
-		output_flat(0) = timestep;
-		output_flat(1) = point;
+    // Execute Point
+    char* output_ptr = (char*)(&output_flat);
+    size_t output_bytes = sizeof(output_flat);
+    size_t n_inputs = 2;
+    size_t input_bytes[n_inputs] = {sizeof(*input[0]), sizeof(*input[1])};
+    const char** input_ptr = (const char**)(input);
 
-		// Execute Point
-		char *output_ptr = (char *)(&output_flat);
-		size_t output_bytes = sizeof(output_flat);
-		size_t n_inputs = 2;
-		size_t input_bytes[n_inputs] = {sizeof(*input[0]), sizeof(*input[1])};
-		const char **input_ptr = (const char **)(input);
-		
-		task_graph_execute_point(task_graph, timestep, point, output_ptr, output_bytes, input_ptr, input_bytes, n_inputs);
-	}
+    task_graph_execute_point(task_graph, timestep, point, output_ptr,
+                             output_bytes, input_ptr, input_bytes, n_inputs);
+  }
 };
 
 // REGISTER_KERNEL_BUILDER(Name("TwoInput").Device(DEVICE_CPU), TwoInputOp);
 
-// Macro for registering operator for different types (int8, int16, int32, int64)
-#define REGISTER_KERNEL(type)											\
-	REGISTER_KERNEL_BUILDER(											\
-		Name("TwoInput").Device(DEVICE_CPU).TypeConstraint<type>("T"),	\
-		TwoInputOp<type>)
+// Macro for registering operator for different types (int8, int16, int32,
+// int64)
+#define REGISTER_KERNEL(type)                                        \
+  REGISTER_KERNEL_BUILDER(                                           \
+      Name("TwoInput").Device(DEVICE_CPU).TypeConstraint<type>("T"), \
+      TwoInputOp<type>)
 
 REGISTER_KERNEL(int8);
 REGISTER_KERNEL(int16);
