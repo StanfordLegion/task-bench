032af3e8153a54b585f34b8d222304bd0d9e0588
Wei Wu <eddy16112@gmail.com>
2019-03-07 18:49:27 -0500

    rework on starpu dag trimming

--- starpu/main.cc
+++ starpu/main.cc
@@ -870,6 +870,41 @@ void StarPUApp::execute_timestep(size_t idx, long t)
     int num_args; 
 #ifdef ENABLE_PRUNE_MPI_TASK_INSERT
     int has_task = 0;   
+    
+    if(desc_islocal(mat.ddescA, t%nb_fields, x)) {
+      has_task = 1;
+    }
+
+    if( t < g.timesteps-1 && has_task != 1 ){
+      long dset_r = g.dependence_set_at_timestep(t+1);
+      std::vector<std::pair<long, long> > rdeps = g.reverse_dependencies(dset_r, x);
+      for (std::pair<long, long> rdep : rdeps) {
+        debug_printf(1, "R: (%d, %d): [%d, %d] \n", x, t, rdep.first, rdep.second); 
+          for (int i = rdep.first; i <= rdep.second; i++) {
+            if(desc_islocal(mat.ddescA, (t+1)%nb_fields, i)) {
+              has_task = 1;
+            }
+          }
+      }
+    }
+    
+    if (deps.size() != 0 && t != 0) {
+      for (std::pair<long, long> dep : deps) {
+        for (int i = dep.first; i <= dep.second; i++) {
+          if(desc_islocal(mat.ddescA, (t-1)%nb_fields, i)) {
+            has_task = 1;
+          }
+        }
+      }
+    }
+            
+
+    debug_printf(1, "rank: %d, has_task: %d, x: %d, t: %d, task_id: %d\n", rank , has_task, x, t, mat.NT * t + x + 1);
+    
+    if (has_task == 0) {
+      continue;
+    }
+
 #endif
     
     if (deps.size() == 0) {
@@ -893,47 +928,16 @@ void StarPUApp::execute_timestep(size_t idx, long t)
           for (int i = dep.first; i <= dep.second; i++) {
             args.push_back(starpu_desc_getaddr( mat.ddescA, (t-1)%nb_fields, i ));
             args_loc.push_back(std::make_pair((t-1)%nb_fields, i));
-#ifdef ENABLE_PRUNE_MPI_TASK_INSERT
-            if(desc_islocal(mat.ddescA, (t-1)%nb_fields, i)) {
-              has_task = 1;
-            }
-#endif
           }
         }
       }
     }
     
-#ifdef ENABLE_PRUNE_MPI_TASK_INSERT
-    if(desc_islocal(mat.ddescA, t%nb_fields, x)) {
-      has_task = 1;
-    }
-
-    if( t < g.timesteps-1 && has_task != 1 ){
-      long dset_r = g.dependence_set_at_timestep(t+1);
-      std::vector<std::pair<long, long> > rdeps = g.reverse_dependencies(dset_r, x);
-      for (std::pair<long, long> rdep : rdeps) {
-        debug_printf(1, "R: (%d, %d): [%d, %d] \n", x, t, rdep.first, rdep.second); 
-          for (int i = rdep.first; i <= rdep.second; i++) {
-            if(desc_islocal(mat.ddescA, (t+1)%nb_fields, i)) {
-              has_task = 1;
-            }
-          }
-      }
-    }
-
-    debug_printf(1, "rank: %d, has_task: %d, x: %d, t: %d, task_id: %d\n", rank , has_task, x, t, mat.NT * t + x + 1);
-#endif
-    
     payload.i = t;
     payload.j = x;
     payload.graph = g;
     payload.graph_id = idx;
-#ifdef ENABLE_PRUNE_MPI_TASK_INSERT
-    if(has_task)
-#endif
-    {
-      insert_task(num_args, payload, args, args_loc); 
-    }
+    insert_task(num_args, payload, args, args_loc); 
     args.clear();
     args_loc.clear();
   }
