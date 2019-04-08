28f75ba573748ff9e23ef603ea6db85a570d6fcd
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-04-08 11:06:55 -0700

    Tweak figure height.

--- scripts/render_all.sh
+++ scripts/render_all.sh
@@ -47,7 +47,7 @@ if [[ $(basename $PWD) = compute ]]; then
 
     "$root_dir"/render_metg.py efficiency_stencil_mpi.csv \
                --legend '' \
-               --height 4 \
+               --height 4.25 \
                --xlabel 'Task Granularity (ms)' \
                --xdata 'time_per_task' \
                --x-invert \
@@ -73,7 +73,7 @@ if [[ $(basename $PWD) = compute ]]; then
 
     "$root_dir"/render_metg.py flops_stencil_mpi.csv \
                --legend '' \
-               --height 4 \
+               --height 4.25 \
                --xlabel 'Problem Size' \
                --xdata 'iterations' \
                --x-invert \
