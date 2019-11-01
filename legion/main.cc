473b31caeb662f5ab6c5c69db7977291bb388117
Elliott Slaughter <slaughter@cs.stanford.edu>
2019-11-01 10:46:08 -0700

    Add linear sharding functor for Legion.

--- legion/main.cc
+++ legion/main.cc
@@ -33,6 +33,10 @@ enum TaskIDs {
   TID_DUMMY,
 };
 
+enum ShardingFunctorIDs {
+  SID_LINEAR = 1,
+};
+
 // In order to avoid spurious WAR dependencies, we round-robin the
 // task output across a set of fields. This is basically
 // double-buffering with an arbitrary factor N. This could technically
@@ -61,11 +65,123 @@ struct Payload {
   IndexPartitionT<1> primary_partition;
 };
 
+class LinearShardingFunctor : public ShardingFunctor {
+public:
+  LinearShardingFunctor();
+  LinearShardingFunctor(const LinearShardingFunctor &rhs);
+  virtual ~LinearShardingFunctor(void);
+public:
+  LinearShardingFunctor& operator=(const LinearShardingFunctor &rhs);
+public:
+  template<int DIM>
+  size_t linearize_point(const Realm::IndexSpace<DIM,coord_t> &is,
+                         const Realm::Point<DIM,coord_t> &point) const;
+public:
+  virtual ShardID shard(const DomainPoint &point,
+                        const Domain &full_space,
+                        const size_t total_shards);
+};
+
+//--------------------------------------------------------------------------
+LinearShardingFunctor::LinearShardingFunctor()
+  : ShardingFunctor()
+//--------------------------------------------------------------------------
+{
+}
+
+//--------------------------------------------------------------------------
+LinearShardingFunctor::LinearShardingFunctor(
+                                           const LinearShardingFunctor &rhs)
+  : ShardingFunctor()
+//--------------------------------------------------------------------------
+{
+  // should never be called
+  assert(false);
+}
+
+//--------------------------------------------------------------------------
+LinearShardingFunctor::~LinearShardingFunctor(void)
+//--------------------------------------------------------------------------
+{
+}
+
+//--------------------------------------------------------------------------
+LinearShardingFunctor& LinearShardingFunctor::operator=(
+                                           const LinearShardingFunctor &rhs)
+//--------------------------------------------------------------------------
+{
+  // should never be called
+  assert(false);
+  return *this;
+}
+
+//--------------------------------------------------------------------------
+template<int DIM>
+size_t LinearShardingFunctor::linearize_point(
+                               const Realm::IndexSpace<DIM,coord_t> &is,
+                               const Realm::Point<DIM,coord_t> &point) const
+//--------------------------------------------------------------------------
+{
+  Realm::AffineLinearizedIndexSpace<DIM,coord_t> linearizer(is);
+  return linearizer.linearize(point);
+}
+
+//--------------------------------------------------------------------------
+ShardID LinearShardingFunctor::shard(const DomainPoint &point,
+                                     const Domain &full_space,
+                                     const size_t total_shards)
+//--------------------------------------------------------------------------
+{
+#ifdef DEBUG_LEGION
+  assert(point.get_dim() == full_space.get_dim());
+#endif
+  size_t domain_size = full_space.get_volume();
+  switch (point.get_dim())
+  {
+    case 1:
+      {
+        const DomainT<1,coord_t> is = full_space;
+        const Point<1,coord_t> p1 = point;
+        return linearize_point<1>(is, p1)  * total_shards / domain_size;
+      }
+    case 2:
+      {
+        const DomainT<2,coord_t> is = full_space;
+        const Point<2,coord_t> p2 = point;
+        return linearize_point<2>(is, p2)  * total_shards / domain_size;
+      }
+    case 3:
+      {
+        const DomainT<3,coord_t> is = full_space;
+        const Point<3,coord_t> p3 = point;
+        return linearize_point<3>(is, p3)  * total_shards / domain_size;
+      }
+    default:
+      assert(false);
+  }
+  return 0;
+}
+
 class TaskBenchMapper : public DefaultMapper
 {
 public:
   TaskBenchMapper(MapperRuntime *rt, Machine machine, Processor local,
                   const char *mapper_name);
+  virtual void select_sharding_functor(
+                                 const MapperContext                ctx,
+                                 const Task&                        task,
+                                 const SelectShardingFunctorInput&  input,
+                                       SelectShardingFunctorOutput& output);
+  virtual void select_sharding_functor(
+                                 const MapperContext                ctx,
+                                 const Copy&                        copy,
+                                 const SelectShardingFunctorInput&  input,
+                                       SelectShardingFunctorOutput& output);
+  virtual void select_sharding_functor(
+                                 const MapperContext                ctx,
+                                 const Fill&                        fill,
+                                 const SelectShardingFunctorInput&  input,
+                                       SelectShardingFunctorOutput& output);
   virtual void default_policy_select_target_processors(MapperContext ctx,
                                                        const Task &task,
                                                        std::vector<Processor> &target_procs);
@@ -85,6 +201,33 @@ TaskBenchMapper::TaskBenchMapper(MapperRuntime *rt, Machine machine, Processor l
                                  const char *mapper_name)
   : DefaultMapper(rt, machine, local, mapper_name)
 {
+}
+
+void TaskBenchMapper::select_sharding_functor(
+                                 const MapperContext                ctx,
+                                 const Task&                        task,
+                                 const SelectShardingFunctorInput&  input,
+                                       SelectShardingFunctorOutput& output)
+{
+  output.chosen_functor = SID_LINEAR;
+}
+
+void TaskBenchMapper::select_sharding_functor(
+                                 const MapperContext                ctx,
+                                 const Copy&                        copy,
+                                 const SelectShardingFunctorInput&  input,
+                                       SelectShardingFunctorOutput& output)
+{
+  output.chosen_functor = SID_LINEAR;
+}
+
+void TaskBenchMapper::select_sharding_functor(
+                                 const MapperContext                ctx,
+                                 const Fill&                        fill,
+                                 const SelectShardingFunctorInput&  input,
+                                       SelectShardingFunctorOutput& output)
+{
+  output.chosen_functor = SID_LINEAR;
 }
 
 void TaskBenchMapper::default_policy_select_target_processors(MapperContext ctx,
@@ -670,6 +813,9 @@ int main(int argc, char **argv)
     registrar.set_leaf();
     Runtime::preregister_task_variant<dummy>(registrar, "dummy");
   }
+
+  LinearShardingFunctor *sharding_functor = new LinearShardingFunctor();
+  Runtime::preregister_sharding_functor(SID_LINEAR, sharding_functor);
 
   Runtime::add_registration_callback(update_mappers);
   return Runtime::start(argc, argv);
