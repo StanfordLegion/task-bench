a9dcc552306b097bda8ee9d7c4d1b44ccf09c6ed
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-09-12 13:51:52 -0700

    More fixes.

--- ci.sh
+++ ci.sh
@@ -13,7 +13,8 @@ if [[ "$(uname)" = "Linux" ]]; then
   sudo apt-get update -qq
   sudo apt-get install -qq mpich libmpich-dev libpcre3-dev binutils-dev
   if [[ $USE_CHAPEL -eq 1 || $USE_REGENT -eq 1 ]]; then
-    sudo apt-get install -qq clang-12 libclang-12-dev llvm-12-dev libedit-dev libncurses5-dev zlib1g-dev
+    sudo apt-get install -qq clang-12 libclang-12-dev libclang-cpp12-dev llvm-12-dev libedit-dev libncurses5-dev zlib1g-dev
+    export CMAKE_PREFIX_PATH=/usr/lib/llvm-12:/usr/share/llvm-12
   fi
   if [[ $USE_OMPSS2 -eq 1 ]]; then
     sudo apt-get install -qq libnuma-dev gperf libboost1.71-dev
