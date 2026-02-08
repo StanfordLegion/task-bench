80be910ef676750326d96f420fed37fdb3262db8
guptapratykshh <pratykshgupta9999@gmail.com>
2026-02-08 21:42:17 +0530

    Integrate hpx_task_bench into test suit
    Signed-off-by: guptapratykshh <pratykshgupta9999@gmail.com>

--- test_all.sh
+++ test_all.sh
@@ -187,6 +187,15 @@ fi
 (if [[ $USE_HPX -eq 1 ]]; then
     source "$HPX_DIR"/env.sh
 
+    # Test hpx_task_bench (fork-join executor, no MPI required)
+    for t in "${extended_types[@]}"; do
+        for k in "${kernels[@]}"; do
+            ./hpx/bin/hpx_task_bench -steps $steps -type $t $k
+            ./hpx/bin/hpx_task_bench -steps $steps -type $t $k -and -steps $steps -type $t $k
+        done
+    done
+
+    # Test hpx_distributed (requires MPI)
     for t in "${extended_types[@]}"; do
         for k in "${kernels[@]}"; do
             mpirun -np 1 ./hpx/bin/hpx_distributed -steps $steps -type $t $k
